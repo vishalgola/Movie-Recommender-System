ef570dc51aa116f2b6007788ed109b48e952604c
vishal prajapati <vishalprajapati935498@gmail.com>
2026-03-29 02:02:33 +0530

    downloading data from googledrive

--- app.py
+++ app.py
@@ -3,73 +3,107 @@ import os
 import pickle
 import requests
 
-# def download_file(url, filename):
-#     if not os.path.exists(filename):
-#         print(f"Downloading {filename}...")
-#         r = requests.get(url)
-#         with open(filename, 'wb') as f:
-#             f.write(r.content)
-#
-# # 🔥 Replace with your actual links
-# movie_url = "https://drive.google.com/file/d/1eKCmH-a0qQNqdUVIdwJuZ5GzpzIkCi3x/view?usp=drive_link"
-# similarity_url = "https://drive.google.com/file/d/1kj3sKQqqYGpaarA10dVeVv-8IifGwP0V/view?usp=drive_link"
-#
-# download_file(movie_url, "../movie_list.pkl")
-# download_file(similarity_url, "../similarity.pkl")
+st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
 
+# ---------- CUSTOM CSS ----------
+st.markdown("""
+<style>
+body {
+    background-color: #0E1117;
+}
+.title {
+    text-align: center;
+    font-size: 42px;
+    font-weight: bold;
+    color: #FF4B4B;
+}
+.subtitle {
+    text-align: center;
+    color: #AAAAAA;
+    margin-bottom: 30px;
+}
+.movie-card {
+    text-align: center;
+}
+.movie-title {
+    font-size: 15px;
+    font-weight: 600;
+    margin-top: 8px;
+}
+.footer {
+    text-align: center;
+    color: gray;
+    margin-top: 50px;
+    font-size: 13px;
+}
+</style>
+""", unsafe_allow_html=True)
+
+# ---------- HEADER ----------
+st.markdown("<div class='title'>🎬 Movie Recommender</div>", unsafe_allow_html=True)
+st.markdown("<div class='subtitle'>Find movies you'll love in seconds</div>", unsafe_allow_html=True)
+
+# ---------- LOAD DATA ----------
+def download_file(url, filename):
+    if not os.path.exists(filename):
+        print(f"Downloading {filename}...")
+        r = requests.get(url)
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+
+# 🔥 Replace with your actual links
+movie_url = "https://drive.google.com/file/d/1eKCmH-a0qQNqdUVIdwJuZ5GzpzIkCi3x/view?usp=drive_link"
+similarity_url = "https://drive.google.com/file/d/1kj3sKQqqYGpaarA10dVeVv-8IifGwP0V/view?usp=drive_link"
+
+download_file(movie_url, "movie_list.pkl")
+download_file(similarity_url, "similarity.pkl")
+movies = pickle.load(open('movie_list.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# ---------- SELECT ----------
+movie_list = movies['title'].values
+selected_movie = st.selectbox("🔍 Choose a movie", movie_list)
+
+
+# ---------- FUNCTIONS ----------
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=bf227a7929754fb9166e0671ce5c5455&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=9c74a2ced9cb1306e713c9eb05d87d92&language=en-US".format(
+        movie_id)
+    data = requests.get(url).json()
     poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    return "https://image.tmdb.org/t/p/w500/" + poster_path
+
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
+    names = []
+    posters = []
+
     for i in distances[1:6]:
-        # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+        posters.append(fetch_poster(movie_id))
+        names.append(movies.iloc[i[0]].title)
 
-    return recommended_movie_names,recommended_movie_posters
-
-
-st.header('Movie Recommender System')
-movies = pickle.load(open('../movie_list.pkl', 'rb'))
-similarity = pickle.load(open('../similarity.pkl', 'rb'))
-
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
-
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+    return names, posters
 
 
+# ---------- BUTTON ----------
+if st.button("🚀 Recommend"):
 
+    with st.spinner("Fetching best recommendations for you..."):
+        names, posters = recommend(selected_movie)
 
+    st.markdown("## 🔥 Recommended Movies")
 
+    cols = st.columns(5)
+
+    for i in range(5):
+        with cols[i]:
+            st.markdown("<div class='movie-card'>", unsafe_allow_html=True)
+            st.image(posters[i], use_container_width=True)
+            st.markdown(f"<div class='movie-title'>{names[i]}</div>", unsafe_allow_html=True)
+            st.markdown("</div>", unsafe_allow_html=True)
+
+# ---------- FOOTER ----------
+st.markdown("<div class='footer'>Made with ❤️ using Streamlit | TMDB API</div>", unsafe_allow_html=True)
